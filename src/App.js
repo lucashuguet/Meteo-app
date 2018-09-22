87483f12e6c9f72e4f130a281f8be17fab9ad69b
Lucas <lucashuguet31@gmail.com>
2018-09-22 22:52:54 +0200

    Master

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import Titles from "./conponents/Titles";
 import Form from "./conponents/Form";
 import Meteo from "./conponents/Meteo";
 
-//eslint-disable-next-line
-const API_KEY = "e7f2a5dbaed3439bbbca71dd640965d3";
-
 class App extends React.Component {
-getWeather = async () => {
-  const api_call = await fetch('http://api.openweathermap.org/data/2.5/weatherq?=Manchester,uk&appid=${API_KEY}&units=metric');
-  const data = await api_call.json();
-  console.logs(data);
-}
   render(){
     return(
       <div>
           <Titles />
-          <Form getWeather={this.getWeather}/>
           <Meteo />
+          <Form />
       </div>
     );
   }
--- src/conponents/Meteo.js
+++ src/conponents/Meteo.js
@@ -1,10 +1,23 @@
 import React from "react";
 
 class Meteo extends React.Component {
+constructor (props) {
+    super(props)
+    this.state = {
+        city: 'Toulouse'
+    }
+    this.fetchWeather()
+}
+
+fetchWeather () {
+     fetch('http://samples.openweathermap.org/data/2.5/weather?q=Toulouse,uk&appid=b6907d289e10d714a6e88b30761fae22')  
+     .then((res => res.json())
+     .then
+}
     render(){
         return(
             <div>
-                Meteo Component                
+                           
             </div>
         );
     }
