eb98d2571373c2b9c47c4e41af51f31c05e49a7c
Lucas <lucashuguet31@gmail.com>
2018-09-23 14:20:55 +0200

    Master

--- src/conponents/Meteo.js
+++ src/conponents/Meteo.js
@@ -6,18 +6,21 @@ constructor (props) {
     this.state = {
         city: 'Toulouse'
     }
-    this.fetchWeather()
+    this.componentDidMount()
 }
 
-fetchWeather () {
-     fetch('http://samples.openweathermap.org/data/2.5/weather?q=Toulouse,uk&appid=b6907d289e10d714a6e88b30761fae22')  
-     .then((res => res.json())
-     .then
+componentDidMount() {
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Toulouse&lang=fr&appid=e7f2a5dbaed3439bbbca71dd640965d3&units=metric")
+ .then(res => res.json())
+ .then(resJson => console.log(resJson))
+    
 }
+ 
     render(){
         return(
             <div>
-                           
+                <p>La température est de </p>
             </div>
         );
     }
